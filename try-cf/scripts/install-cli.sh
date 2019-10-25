b354fe3ef2a7d0d4c49ae022137f12acefcbe983
Steve Greenberg <spgreenberg@gmail.com>
2019-10-25 09:38:41 -0600

    insall cf autocomplete

--- try-cf/scripts/install-cli.sh
+++ try-cf/scripts/install-cli.sh
@@ -4,8 +4,4 @@ curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=
 
 mv cf /usr/local/bin
 
- 
-
-# sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
-
-# curl https://http-dump.cfapps.io
+sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
