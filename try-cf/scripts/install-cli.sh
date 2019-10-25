fcc929213714ebd1e6a10b5960213b421aa0c356
Steve Greenberg <spgreenberg@gmail.com>
2019-10-25 09:47:10 -0600

    move and chmod as root

--- try-cf/scripts/install-cli.sh
+++ try-cf/scripts/install-cli.sh
@@ -2,6 +2,8 @@
 
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 
-mv cf /usr/local/bin
+sudo mv cf /usr/local/bin
+
+sudo chmod +x /usr/local/bin/cf
 
 sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
